65b7ab2cc09670df277d48b030d5ef1cd7d47561
Mark <mark.ianadlao@gmail.com>
2020-01-27 11:07:02 -0500

    sandbox insertion

---
+++ sandbox.java
@@ -0,0 +1,19 @@
+public class Sandbox {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("/n Hello, World, this is my sandbox program yes");
+
+        final double PI = 3.14159;
+
+        double radius = 3.5;
+        double area;
+
+        area = PI * radius * radius;
+
+        System.out.println("the area of the circle is " + area);
+       
+
+    }
+
+}
